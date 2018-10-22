01dbcfa55c50f55e4253dd179d49f2a652c390c3
LAPTOP-8SFLP197\walla <wallace.ballentine@gmail.com>
2018-10-21 21:36:46 -0600

    This section covered objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,4 @@
+// Write your fasterShip object literal below
+let fasterShip = {color: 'silver', 'Fuel Type': 'Turbo Fuel'};
+
+//I defined an object using braces and key value pairs
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,15 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines = 10;
+
+delete spaceship['Secret Mission'];
+
+//I assigned new properties with dot notation and deleted a property
